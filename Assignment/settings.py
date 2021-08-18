baf46bd8d01d35160a9d6d869e408dbb9aab6aec
BenK93 <benkoren23@gmail.com>
2021-08-18 09:21:17 +0300

    adding static root and django-heroku

--- Assignment/settings.py
+++ Assignment/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from decouple import config
-# import django_heroku
+import django_heroku
 import os
 from pathlib import Path
 
@@ -134,10 +134,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
